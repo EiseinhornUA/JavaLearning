44726c9bd43a5ebec9023cec255ea1e3c8a115d3
EiseinhornUA <kereya@gmail.com>
2016-05-10 23:48:17 +0300

    1st commit
    initial commit

---
+++ 2nd project/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+   static public void main(String[] args)
+    {
+        System.out.println("Hello World!");
+
+        Main programm = new Main();
+        programm.start();
+
+    }
+
+    public void start()
+    {
+        System.out.println(  Variables.i );
+        System.out.println(  Variables.x );
+        System.out.println(  Variables.symb );
+        System.out.println(  Variables.Hello );
+    }
+}
---
+++ 2nd project/src/Variables.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Евгений Керея on 10.05.2016.
+ */
+public class Variables {
+
+   static int i = 10;
+    static double x = 20;
+    static char symb = 'S';
+    static String Hello = "Hi Catalyst from Java developer";
+
+    public void PrintFunction() {
+        System.out.println(this.i);
+    }
+}
